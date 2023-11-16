cfcc347d326fa8c977ab950d4e58d5666269f89e
Maxim Derbenev <maksym.derbenov@hit.zone>
2023-11-16 16:29:47 -0500

    first commit

---
+++ providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "aws_workshop"
+  default_tags {
+    tags = local.tags
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.67.0"
+    }
+  }
+
+  required_version = ">= 1.4.2"
+}
